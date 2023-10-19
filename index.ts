162ef76d4f05da7303df62f69c588a335eb8d398
yisus82 <djalma8@gmail.com>
2023-10-19 12:04:42 +0200

    Create a web server

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import figlet from 'figlet';
+
+const server = Bun.serve({
+  port: 3000,
+  fetch(_request, _server) {
+    const body = figlet.textSync('Hello Bun!!!', {
+      font: 'Star Wars',
+      width: 80,
+      whitespaceBreak: true,
+    });
+    return new Response(body);
+  },
+});
+
+console.log(`Listening on http://localhost:${server.port}`);
