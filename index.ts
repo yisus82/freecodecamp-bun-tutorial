87d46ab5a5efca82baa28630da6adefa9c2ed929
yisus82 <djalma8@gmail.com>
2023-10-19 13:19:26 +0200

    Creating routes

--- index.ts
+++ index.ts
@@ -2,13 +2,27 @@ import figlet from 'figlet';
 
 const server = Bun.serve({
   port: 3000,
-  fetch(_request, _server) {
-    const body = figlet.textSync('Hello Bun!!!', {
-      font: 'Star Wars',
-      width: 80,
-      whitespaceBreak: true,
-    });
-    return new Response(body);
+  fetch(request, _server) {
+    const url = new URL(request.url);
+
+    if (url.pathname === '/') {
+      const body = figlet.textSync('Hello Bun!!!', {
+        font: 'Star Wars',
+        width: 80,
+        whitespaceBreak: true,
+      });
+      return new Response(body);
+    }
+
+    if (url.pathname === '/about') {
+      return new Response('This is Bun!');
+    }
+
+    if (url.pathname === '/contact') {
+      return new Response('Contact us at bunisgreat@bun.com');
+    }
+
+    return new Response('Not found', { status: 404 });
   },
 });
 
