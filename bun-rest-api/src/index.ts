6d5fa24a417eddc30f66119d14d4df848ab1f505
yisus82 <djalma8@gmail.com>
2023-10-21 13:47:01 +0200

    Add routes and handlers

--- bun-rest-api/src/index.ts
+++ bun-rest-api/src/index.ts
@@ -1,5 +1,20 @@
 import { Elysia } from 'elysia';
 
-const app = new Elysia().get('/', () => 'Hello Elysia 🦊').listen(3000);
+const app = new Elysia()
+  .get('/', () => 'Hello Elysia 🦊')
+  .get('/post/:id', ({ params: { id } }) => ({ id, title: `Learn Bun (Part ${id})` }))
+  .post('/post', ({ body, set }) => {
+    set.status = 201;
+    return body;
+  })
+  .get('/track/*', () => 'Track Route')
+  .get('/tracks', () => ({
+    tracks: [
+      { id: 1, title: 'Learn Bun (Part 1)' },
+      { id: 2, title: 'Learn Bun (Part 2)' },
+      { id: 3, title: 'Learn Bun (Part 3)' },
+    ],
+  }))
+  .listen(3000);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
