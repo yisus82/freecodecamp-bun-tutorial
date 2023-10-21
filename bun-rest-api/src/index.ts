ec395b1101365e83aef00dc57738f090482ac3cd
yisus82 <djalma8@gmail.com>
2023-10-21 16:34:53 +0200

    State and decorate

--- bun-rest-api/src/index.ts
+++ bun-rest-api/src/index.ts
@@ -1,6 +1,10 @@
 import { Elysia } from 'elysia';
 
 const app = new Elysia()
+  .state({
+    version: '1.0.0',
+  })
+  .decorate('getDate', () => new Date())
   .get('/', () => 'Hello Elysia 🦊')
   .get('/post/:id', ({ params: { id } }) => ({ id, title: `Learn Bun (Part ${id})` }))
   .post('/post', ({ body, set }) => {
@@ -8,12 +12,14 @@ const app = new Elysia()
     return body;
   })
   .get('/track/*', () => 'Track Route')
-  .get('/tracks', () => ({
+  .get('/tracks', ({ store, getDate }) => ({
     tracks: [
       { id: 1, title: 'Learn Bun (Part 1)' },
       { id: 2, title: 'Learn Bun (Part 2)' },
       { id: 3, title: 'Learn Bun (Part 3)' },
     ],
+    date: getDate().toDateString(),
+    version: store.version,
   }))
   .listen(3000);
 
