0032c6441b9379281ef58229f382292bf08da637
yisus82 <djalma8@gmail.com>
2023-10-21 18:58:27 +0200

    Group routes

--- bun-rest-api/src/index.ts
+++ bun-rest-api/src/index.ts
@@ -7,23 +7,32 @@ const app = new Elysia()
     version: '1.0.0',
   })
   .decorate('getDate', () => new Date())
-  .get('/', () => 'Hello Elysia 🦊')
-  .get('/post/:id', ({ params: { id } }) => ({ id, title: `Learn Bun (Part ${id})` }))
-  .post('/post', ({ body, set }) => {
-    set.status = 201;
-    return body;
-  })
-  .get('/track/*', () => 'Track Route')
-  .get('/tracks', ({ store, getDate }) => ({
-    tracks: [
-      { id: 1, title: 'Learn Bun (Part 1)' },
-      { id: 2, title: 'Learn Bun (Part 2)' },
-      { id: 3, title: 'Learn Bun (Part 3)' },
-    ],
-    date: getDate().toDateString(),
-    version: store.version,
-    pluginVersion: store['plugin-version'],
-  }))
-  .listen(3000);
+  .get('/', () => 'Hello Elysia 🦊');
+
+app.group('/posts', app =>
+  app
+    .get('/:id', ({ params: { id } }) => ({ id, title: `Learn Bun (Part ${id})` }))
+    .post('/', ({ body, set }) => {
+      set.status = 201;
+      return body;
+    })
+);
+
+app.group('/tracks', app =>
+  app
+    .get('/*', () => 'Track Route')
+    .get('/', ({ store, getDate }) => ({
+      tracks: [
+        { id: 1, title: 'Learn Bun (Part 1)' },
+        { id: 2, title: 'Learn Bun (Part 2)' },
+        { id: 3, title: 'Learn Bun (Part 3)' },
+      ],
+      date: getDate().toDateString(),
+      version: store.version,
+      pluginVersion: store['plugin-version'],
+    }))
+);
+
+app.listen(3000);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
